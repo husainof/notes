cec0deb56fbfa5fdb3afbc6c3a0138f3793765f5
radik <linuxoid.ssh@gmail.com>
2023-04-12 13:25:38 +0400

    front-setup

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import { Box, Text } from "@chakra-ui/react";
+
+function App() {
+  return (
+  <Box bg={"black"} h={"100vh"}>
+    <Text color={"white"}>Hello</Text>
+  </Box>);
+}
+
+export default App;
